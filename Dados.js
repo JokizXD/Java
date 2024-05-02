69947822a182d8a4d02c22394746a1539943708e
JokizXD <jghgamer22@gmail.com>
2024-05-02 16:26:36 -0400

    oie

---
+++ Dados.js
@@ -0,0 +1,26 @@
+{
+    let nome = "Wanderson";
+let verdadeiro = true;
+let falso = false;
+let numero = "40";
+let reais = 25.99;
+let indefinido;
+let NotANumber = 0 / 0;
+let objeto = {
+    name: "Wanderson",
+    age: 40,
+    Profession: "Teacher"
+}}
+
+{
+    var valor = 99;
+var valor = "Centro e trinte";
+console.log(valor); //Saida: Cento e trinta
+}
+
+{
+    let x = "A resposta é" + 42;
+let y = 42 + "é a resposta";
+console.log(x);
+console.log(y);
+}
---
+++ const.js
@@ -0,0 +1,2 @@
+const PI = 3.14; // Inicializado com o valor de 3.14
+const MASSA; //Erro: declaração 'const' devem ser inicializadas
