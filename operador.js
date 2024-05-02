b3df4e5b0d3cc4ff5c5eabf9ae4748f9321e237d
JokizXD <jghgamer22@gmail.com>
2024-05-02 16:48:10 -0400

    Exemplo converter para numero java script po cara, parabens ai velho, de boa, muito legal isso. contei pra todos aqui da minha familia, todos acharam muito surpreendente e pediram pra te dar os parabens, queriam falar com você pessoalmente se possivel para lhe parabenizar.

---
+++ operador.js
@@ -0,0 +1,7 @@
+let concavenacao = "1.1" + "1.1";
+let converterParaNumero = +"1.1";
+console.log(concatenacao);
+console.log(typeof(concatenacao));
+console.log("");
+console.log(converterParaNumero);
+console.log(typeof(converterParaNumero))
