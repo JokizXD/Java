738192846754d12015e8989fac18ab2f9cebea37
JokizXD <jghgamer22@gmail.com>
2024-05-07 15:33:36 -0400

    comparacao de strings

---
+++ Operadores/comparacao-strings.js
@@ -0,0 +1,8 @@
+let stringsIguais = "Wanderson" == "Wanderson";
+let stringsDiferentes = "wanderson" == "Wanderson";
+let stringMenos = "A" < "B";
+let stringMaior = "C" > "D";
+console.log(`Wanderson == Wanderson: ${stringsIguais}`);
+console.log(`wanderson == Wanderson: ${stringsDiferentes}`);
+console.log(`A < B: ${stringMenor}`);
+console.log(`C > D: ${stringMaior}`);
