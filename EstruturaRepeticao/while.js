1addcc1f5a671ba698df8e171ead706b56f4804a
JokizXD <jghgamer22@gmail.com>
2024-05-09 16:12:00 -0400

    trabalho while no java script

---
+++ EstruturaRepeticao/while.js
@@ -0,0 +1,11 @@
+let resultadoDado;
+let lancamentos = 0;
+
+while (resultadoDado !== 6) {
+resultadoDado = Math. floor(Math.random() * 6) + 1;
+lancamentos++;
+console. log( Lancamento ${lancamentos}: Resultado do dado: ${resultadoDado} );
+
+}
+
+console. log( Finalmente! 0 número 6 foi obtido após ${lancamentos} lançamentos. );
