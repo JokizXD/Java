43016295aa62a6065f05df6d51b377b42f5ae3dc
JokizXD <jghgamer22@gmail.com>
2024-05-09 16:14:36 -0400

    corrigindo erros de digitação

--- EstruturaRepeticao/while.js
+++ EstruturaRepeticao/while.js
@@ -4,8 +4,8 @@ let lancamentos = 0;
 while (resultadoDado !== 6) {
 resultadoDado = Math. floor(Math.random() * 6) + 1;
 lancamentos++;
-console. log( Lancamento ${lancamentos}: Resultado do dado: ${resultadoDado} );
+console. log(`Lancamento ${lancamentos}: Resultado do dado: ${resultadoDado}`);
 
 }
 
-console. log( Finalmente! 0 número 6 foi obtido após ${lancamentos} lançamentos. );
+console. log( `Finalmente! 0 número 6 foi obtido após ${lancamentos} lançamentos. `);
